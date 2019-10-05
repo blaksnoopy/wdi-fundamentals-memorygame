24b97ab17528797c4ce25a23dcb76c64501b991f
Marquiese Solomon <blaksnoopy@marquieses-mbp.attlocal.net>
2019-10-04 17:54:57 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,28 +1,52 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
 function checkForMatch(){
-	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-}
 };
 
 function flipCard(cardId){
-   checkForMatch();
- 
-   
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+          checkForMatch();
 }
+    console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+};
+
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
 
 
 
